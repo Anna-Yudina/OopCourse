daff0b475591ff7de532f8666a7e9d3c0468944d
Anna Yudina <yudina@ntk.novotelecom.ru>
2022-02-07 23:19:25 +0700

    CSVProject начало

---
+++ CSVProject/src/ru/academits/yudina/CSVProject/CSVProject.java
@@ -0,0 +1,35 @@
+package ru.academits.yudina.CSVProject;
+
+import java.io.*;
+
+public class CSVProject {
+    public static void main(String[] args) {
+        String inputFileName = "H:/Аня/работа/помойка/CSVProject.csv";
+        String outputFileName = "H:/Аня/работа/помойка/OutputFile.html";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
+            try (BufferedWriter writter = new BufferedWriter(new FileWriter(outputFileName))) {
+                String line;
+                writter.write("<table>" + System.lineSeparator());
+
+                while ((line = reader.readLine()) != null) {
+                    String[] data = line.split(";");
+                    writter.write("    <tr>" + System.lineSeparator());
+
+                    for (String element : data) {
+                        writter.write("        <td>" + element + "</td>" + System.lineSeparator());
+                    }
+
+                    writter.write("    </tr>" + System.lineSeparator());
+                }
+
+                writter.write("</table>" + System.lineSeparator());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
