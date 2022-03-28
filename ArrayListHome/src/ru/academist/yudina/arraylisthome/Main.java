08cd3cf864e878fd691c2276e1855378dd881687
Anna-Yudina <anuta-nuty@yandex.ru>
2022-03-28 21:43:05 +0700

    Homework ArrayListHome: fixed 4 mistakes

--- ArrayListHome/src/ru/academist/yudina/arraylisthome/Main.java
+++ ArrayListHome/src/ru/academist/yudina/arraylisthome/Main.java
@@ -17,16 +17,18 @@ public class Main {
                 fileLinesList.add(line);
                 line = reader.readLine();
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
         } catch (IOException e) {
             System.out.println("Ошибка ввода-вывода");
         }
 
-        System.out.println("Вывод содержимого файла: ");
+        System.out.println("Вывод содержимого файла:");
         System.out.println(fileLinesList);
         System.out.println("---------------");
 
-        //2. Есть список из целых чисел. Удалить из него все четные числа. В
-        //этой задаче новый список создавать нельзя
+        // 2. Есть список из целых чисел. Удалить из него все четные числа. В
+        // этой задаче новый список создавать нельзя
         System.out.println("Необходимо из списка удалить все четные числа.");
         ArrayList<Integer> task2NumbersList = new ArrayList<>(Arrays.asList(1, 6, 8, 9, 5, 4, 10, 11));
         System.out.println("Список до изменений: " + task2NumbersList);
@@ -41,14 +43,15 @@ public class Main {
         System.out.println("Список после изменений: " + task2NumbersList);
         System.out.println("---------------");
 
-        //3. Есть список из целых чисел, в нём некоторые числа могут повторяться.
+        // 3. Есть список из целых чисел, в нём некоторые числа могут повторяться.
         // Надо создать новый список, в котором будут элементы первого списка в таком же порядке, но без повторений
-        //Например, был список [1, 5, 2, 1, 3, 5], должен получиться новый список [1, 5, 2, 3]
+        // Например, был список [1, 5, 2, 1, 3, 5], должен получиться новый список [1, 5, 2, 3]
         System.out.println("Необходимо из списка удалить все повторения.");
         ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(1, 5, 2, 1, 3, 5));
         System.out.println("Дан список чисел: " + numbersList);
 
         ArrayList<Integer> noRepetitionsNumbersList = new ArrayList<>(numbersList.size());
+
         for (Integer number : numbersList) {
             if (!noRepetitionsNumbersList.contains(number)) {
                 noRepetitionsNumbersList.add(number);
