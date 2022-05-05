49432ed44c0583416c09f4671184f287d406c134
Anna-Yudina <anuta-nuty@yandex.ru>
2022-05-05 23:20:14 +0700

    Project Matrix

--- Matrix/src/ru/academits/yudina/matrix/Matrix.java
+++ Matrix/src/ru/academits/yudina/matrix/Matrix.java
@@ -2,12 +2,14 @@ package ru.academits.yudina.matrix;
 
 import ru.academits.yudina.vector.Vector;
 
-import java.util.Arrays;
-
 public class Matrix {
     private Vector[] vectors;
 
     public Matrix(int line, int column) {
+        if (line <= 0) {
+            throw new IllegalArgumentException("Введенное количество строк в матрице " + line + " не верное. Число должно быть > 0");
+        }
+
         vectors = new Vector[line];
 
         for (int i = 0; i < line; i++) {
@@ -16,6 +18,8 @@ public class Matrix {
     }
 
     public Matrix(double[][] dimension) {
+        //TODO добить матрицу нулями
+
         vectors = new Vector[dimension.length];
 
         for (int i = 0; i < dimension.length; i++) {
@@ -25,12 +29,12 @@ public class Matrix {
 
     public Matrix(Matrix matrix) {
         vectors = new Vector[matrix.vectors.length];
-        for (int i = 0; i < matrix.vectors.length; i++) {
-            vectors[i] = matrix.vectors[i];
-        }
+        System.arraycopy(matrix.vectors, 0, vectors, 0, matrix.vectors.length);
     }
 
     public Matrix(Vector[] vectors) {
+        //TODO добить матрицу нулями
+
         this.vectors = vectors;
     }
 
@@ -41,20 +45,14 @@ public class Matrix {
 
     // получение вектора-строки по индексу
     public Vector getVectorLine(int index) {
-        if (index < 0 || index >= vectors.length) {
-            throw new IndexOutOfBoundsException("Индекс " + index + " выходит за пределы размерности массива векторов. " +
-                    "Допустимые значения должны быть в диапазоне [0, " + (vectors.length - 1) + "].");
-        }
+        checkIndex(index);
 
         return vectors[index];
     }
 
     // задание вектора-строки по индексу
     public void setVectorLine(int index, Vector vector) {
-        if (index < 0 || index >= vectors.length) {
-            throw new IndexOutOfBoundsException("Индекс " + index + " выходит за пределы размерности массива векторов. " +
-                    "Допустимые значения должны быть в диапазоне [0, " + (vectors.length - 1) + "].");
-        }
+        checkIndex(index);
 
         vectors[index] = vector;
     }
@@ -76,28 +74,22 @@ public class Matrix {
     }
 
     // транспонирование матрицы
-    // TODO дописать метод, транспонирование - это столбцы делаем строками и наоборот
-    public Vector[] transpose(){
-        if (vectors.length < vectors[0].getSize()){
-            for (int i = vectors.length; i < vectors[0].getSize(); i++){
-                vectors[i] = new Vector(vectors[0].getSize());
-            }
+    public Matrix transpose() {
+
+        Matrix transposeMatrix = new Matrix(vectors[0].getSize(), vectors.length);
+
+        for (int i = 0; i < vectors[0].getSize(); i++) {
+            Vector vector = this.getVectorColumn(i);
+            transposeMatrix.setVectorLine(i, vector);
         }
 
-        for (int i = 0; i < vectors.length; i++) {
-            for (int j = i; j < vectors.length; j++) {
-                double tmp = vectors[j].getElement(i);
-                vectors[j].setElement(i, vectors[i].getElement(j));
-                vectors[i].setElement(j, tmp);
-            }
-        }
-        return vectors;
+        return transposeMatrix;
     }
 
     // умножение на скаляр
     public void multiplyByScalar(double number) {
-        for (int i = 0; i < vectors.length; i++) {
-            vectors[i].multiplyByScalar(number);
+        for (Vector vector : vectors) {
+            vector.multiplyByScalar(number);
         }
     }
 
@@ -106,13 +98,16 @@ public class Matrix {
 
     // умножение матрицы на вектор
     public Vector multiplyMatrixAndVector(Vector vector) {
+        // TODO дописать, если передали нулевой вектор
+
+        // TODO дописать условие, если количество столбцов матрицы не равно количеству элементов в векторе
         Vector newVector = new Vector(vectors.length);
 
         for (int i = 0; i < vectors.length; i++) {
             double sum = 0;
 
             for (int j = 0; j < vectors[i].getSize(); j++) {
-                sum = sum + vectors[i].getElement(j)*vector.getElement(j);
+                sum = sum + vectors[i].getElement(j) * vector.getElement(j);
             }
 
             newVector.setElement(i, sum);
@@ -122,15 +117,67 @@ public class Matrix {
     }
 
     // сложение матриц
-    public void add (Matrix matrix){
-        for (int i = 0; i< vectors.length; i++){
+    public void add(Matrix matrix) {
+        if (matrix.vectors.length != vectors.length && matrix.vectors[0].getSize() != vectors[0].getSize()) {
+            increaseMatrix(matrix);
+        }
+
+        for (int i = 0; i < matrix.vectors.length; i++) {
             vectors[i].add(matrix.vectors[i]);
         }
     }
 
+    // метод увеличения текущей матрицы в зависимости от размеров входящей матрицы
+    public void increaseMatrix(Matrix matrix) {
+        int inputMatrixLine = matrix.vectors.length;
+        int inputMatrixColumn = matrix.vectors[0].getSize();
+        int currentMatrixLine = vectors.length;
+        int currentMatrixColumn = vectors[0].getSize();
+
+        Matrix increasedMatrix;
+
+        if (inputMatrixLine > currentMatrixLine) {
+            if (inputMatrixColumn > currentMatrixColumn) {
+                increasedMatrix = getIncreasedMatrix(inputMatrixLine, inputMatrixColumn);
+            } else {
+                increasedMatrix = getIncreasedMatrix(inputMatrixLine, currentMatrixColumn);
+            }
+        } else {
+            if (inputMatrixColumn > currentMatrixColumn) {
+                increasedMatrix = getIncreasedMatrix(currentMatrixLine, inputMatrixColumn);
+            } else {
+                return;
+            }
+        }
+
+        this.vectors = increasedMatrix.vectors;
+    }
+
+    // вспомогательный метод для получения увеличенной матрицы до размеров, переданных в аргументе
+    private Matrix getIncreasedMatrix(int line, int column) {
+        Matrix increasedMatrix = new Matrix(line, column);
+
+        for (int i = 0; i < vectors.length; i++) {
+            increasedMatrix.vectors[i] = new Vector(column);
+            double[] numbers = new double[column];
+
+            for (int j = 0; j < vectors[0].getSize(); j++) {
+                numbers[j] = vectors[i].getElement(j);
+            }
+
+            increasedMatrix.vectors[i] = new Vector(column, numbers);
+        }
+
+        return increasedMatrix;
+    }
+
     // вычитание матриц
-    public void subtract (Matrix matrix){
-        for (int i = 0; i< vectors.length; i++){
+    public void subtract(Matrix matrix) {
+        if (matrix.vectors.length != vectors.length && matrix.vectors[0].getSize() != vectors[0].getSize()) {
+            increaseMatrix(matrix);
+        }
+
+        for (int i = 0; i < matrix.vectors.length; i++) {
             vectors[i].subtract(matrix.vectors[i]);
         }
     }
@@ -140,11 +187,37 @@ public class Matrix {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("{");
 
-        for (int i = 0; i < vectors.length; i++) {
-            stringBuilder.append(vectors[i]);
+        for (Vector vector : vectors) {
+            stringBuilder.append(vector);
         }
 
         stringBuilder.append('}');
         return stringBuilder.toString();
+    }
+
+    public void checkIndex(int index) {
+        if (index < 0 || index >= vectors.length) {
+            throw new IndexOutOfBoundsException("Индекс " + index + " выходит за пределы размерности массива векторов. " +
+                    "Допустимые значения должны быть в диапазоне [0, " + (vectors.length - 1) + "].");
+        }
+    }
+
+    public static Matrix getSum(Matrix matrix1, Matrix matrix2) {
+        Matrix resultMatrix = new Matrix(matrix1);
+        resultMatrix.add(matrix2);
+        return resultMatrix;
+    }
+
+    public static Matrix getDifference(Matrix matrix1, Matrix matrix2) {
+        Matrix resultMatrix = new Matrix(matrix1);
+        resultMatrix.subtract(matrix2);
+        return resultMatrix;
+    }
+
+    public static Matrix getMultiply(Matrix matrix1, Matrix matrix2) {
+        // TODO добавить условие
+        Matrix resultMatrix = new Matrix(matrix1);
+        // TODO написать цикл, используя метод умножения матрицы на вектор
+        return resultMatrix;
     }
 }
--- Matrix/src/ru/academits/yudina/matrix_main/Main.java
+++ Matrix/src/ru/academits/yudina/matrix_main/Main.java
@@ -4,22 +4,32 @@ package ru.academits.yudina.matrix_main;
 import ru.academits.yudina.matrix.Matrix;
 import ru.academits.yudina.vector.Vector;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Создаем матрицу нулей размера 4*7:");
         Matrix matrix1 = new Matrix(4, 7);
         System.out.println(matrix1);
 
-        double[][] dimension = {
+        System.out.println("_______________");
+        System.out.println("Создаем матрицу из двумерного массива:");
+        double[][] dimension1 = {
                 {5, 7, 3, 17},
                 {7, 0, 1, 12},
                 {8, 1, 2, 3}
         };
-        Matrix matrix2 = new Matrix(dimension);
+        Matrix matrix2 = new Matrix(dimension1);
         System.out.println(matrix2);
 
+        System.out.println("_______________");
+        System.out.println("Создаем матрицу с помощью конструктора копирования:");
         Matrix matrix3 = new Matrix(matrix2);
         System.out.println(matrix3);
 
+        System.out.println("_______________");
+        System.out.println("Создаем матрицу из массива векторов-строк:");
         Vector[] vectors1 = new Vector[3];
         vectors1[0] = new Vector(new double[]{1, 2, 3, 4});
         vectors1[1] = new Vector(new double[]{5, 6, 7, 8});
@@ -28,33 +38,96 @@ public class Main {
         Matrix matrix4 = new Matrix(vectors1);
         System.out.println(matrix4);
 
+        System.out.println("_______________");
+        System.out.println("Проверяем метод String getMatrixSize():");
         System.out.println("Размеры матрицы: " + matrix2.getMatrixSize());
 
-        Vector vector1 = matrix2.getVectorLine(1);
+        System.out.println("_______________");
+        System.out.println("Проверяем метод Vector getVectorLine(int index):");
+        System.out.println("Матрица: " + matrix2);
+        Vector vector1 = matrix2.getVectorLine(2);
         System.out.println(vector1);
 
+        System.out.println("_______________");
+        System.out.println("Проверяем метод Vector getVectorColumn(int index):");
+        System.out.println("Матрица: " + matrix2);
         Vector vector2 = matrix2.getVectorColumn(1);
-        System.out.println(vector2);
+        System.out.println("Получили вектор-столбец по индексу 1: " + vector2);
 
+        System.out.println("_______________");
+        System.out.println("Проверяем метод multiplyByScalar(double number):");
+        System.out.println("Матрица: " + matrix2);
         matrix2.multiplyByScalar(2);
-        System.out.println(matrix2);
+        System.out.println("Матрица после умножения на скаляр 2: " + matrix2);
 
+        System.out.println("_______________");
+        System.out.println("Проверяем метод void setVectorLine(int index, Vector vector):");
+        System.out.println("Матрица: " + matrix2);
         Vector vector3 = new Vector(new double[]{5, 7, 8, 3});
+        System.out.println("Вектор: " + vector3);
         matrix2.setVectorLine(1, vector3);
-        System.out.println(matrix2);
+        System.out.println("Матрица после вставки вектора по индексу 1: " + matrix2);
 
+        System.out.println("_______________");
+        System.out.println("Проверяем метод Vector multiplyMatrixAndVector(Vector vector):");
         Vector vector4 = new Vector(new double[]{2, 3, 4, 6});
+        System.out.println("Матрица: " + matrix2);
+        System.out.println("Вектор: " + vector4);
         Vector vector5 = matrix2.multiplyMatrixAndVector(vector4);
-        System.out.println(vector5);
-
-        System.out.println(matrix2);
-        System.out.println(matrix4);
-        matrix2.subtract(matrix4);
-        System.out.println(matrix2);
+        System.out.println("Результат перемножения матрицы на вектор: " + vector5);
 
         System.out.println("___________________");
-        System.out.println(matrix2);
-        matrix2.transpose();
-        System.out.println(matrix2);
+        System.out.println("Проверка метода transpose():");
+        System.out.println("Транспонируем матрицу: " + matrix2);
+        Matrix matrix5 = matrix2.transpose();
+        System.out.println("Полученный результат: " + matrix5);
+
+        System.out.println("___________________");
+        System.out.println("Проверка метода void add(Matrix matrix):");
+        System.out.println("Первая матрица: " + matrix2);
+        System.out.println("Вторая матрица: " + matrix4);
+        matrix2.add(matrix4);
+        System.out.println("Получившийся результат сложения двух матриц: " + matrix2);
+
+        System.out.println("Проверка метода add, если матрицы не равны:");
+
+        double[][] dimension2 = {
+                {5, 7, 3},
+                {7, 0, 1},
+                {8, 1, 2},
+                {4, 8, 0},
+                {9, 6, 5}
+        };
+
+        Matrix matrix6 = new Matrix(dimension2);
+        System.out.println("Первая матрица: " + matrix2);
+        System.out.println("Вторая матрица: " + matrix6);
+        matrix2.add(matrix6);
+        System.out.println("Результат после сложения: " + matrix2);
+
+        System.out.println("_______________");
+        System.out.println("Проверяем метод void subtract (Matrix matrix):");
+        System.out.println("Первая матрица: " + matrix3);
+        System.out.println("Вторая матрица: " + matrix6);
+        matrix3.subtract(matrix6);
+        System.out.println("Результат вычитания: " + matrix3);
+
+        System.out.println("_______________");
+        System.out.println("Проверяем статический метод getSum:");
+        System.out.println("Первая матрица: " + matrix2);
+        System.out.println("Вторая матрица: " + matrix6);
+        Matrix resultMatrix1 = Matrix.getSum(matrix2, matrix6);
+        System.out.println("Результат после сложения: " + resultMatrix1);
+
+        System.out.println("_______________");
+        System.out.println("Проверяем статический метод getDifference:");
+        System.out.println("Первая матрица: " + matrix3);
+        System.out.println("Вторая матрица: " + matrix6);
+        Matrix resultMatrix2 = Matrix.getDifference(matrix3, matrix6);
+        System.out.println("Результат вычитания: " + resultMatrix2);
+
+        double [][] twoDimArray = {{5,7,3}, {7,0}, {8,1,2,3}};
+        System.out.println(Arrays.deepToString(twoDimArray));
+        Matrix matrix7 = new Matrix(twoDimArray);
     }
 }
