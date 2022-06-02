12b755eebb1e76246f8fe950946aa41e2e6bf02e
Anna-Yudina <anuta-nuty@yandex.ru>
2022-06-02 22:02:15 +0700

    Project Matrix: fixed 6 problem

--- CsvToHtmlConverter/src/ru/academits/yudina/csv_to_html_converter/CsvToHtmlConverter.java
+++ CsvToHtmlConverter/src/ru/academits/yudina/csv_to_html_converter/CsvToHtmlConverter.java
@@ -3,60 +3,60 @@ package ru.academits.yudina.csv_to_html_converter;
 import java.io.*;
 
 public class CsvToHtmlConverter {
-    private String inputFileName;
-    private String outputFileName;
+    private String inputFilePath;
+    private String outputFilePath;
 
-    public void setInputFileName(String inputFileName) {
-        this.inputFileName = inputFileName;
+    public void setInputFilePath(String inputFilePath) {
+        this.inputFilePath = inputFilePath;
     }
 
-    public void setOutputFileName(String outputFileName) {
-        this.outputFileName = outputFileName;
+    public void setOutputFilePath(String outputFilePath) {
+        this.outputFilePath = outputFilePath;
     }
 
     public CsvToHtmlConverter(String inputFileName, String outputFileName) {
-        this.inputFileName = inputFileName;
-        this.outputFileName = outputFileName;
+        this.inputFilePath = inputFileName;
+        this.outputFilePath = outputFileName;
     }
 
-    public void writeHtmlFile() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
-            writer.write("<!DOCTYPE html>" + System.lineSeparator() + System.lineSeparator());
+    public void writeHtmlFile(BufferedReader reader, BufferedWriter writer) {
+        try {
+            writer.write("<!DOCTYPE html>" + System.lineSeparator());
             writer.write("<html>" + System.lineSeparator());
-            writer.write("  <head>" + System.lineSeparator());
-            writer.write("  <meta charset=\"UTF-8\">" + System.lineSeparator());
-            writer.write("  </head>" + System.lineSeparator() + System.lineSeparator());
-            writer.write("  <body>" + System.lineSeparator());
-            writer.write("    <table>" + System.lineSeparator());
+            writer.write("    <head>" + System.lineSeparator());
+            writer.write("        <meta charset=\"UTF-8\">" + System.lineSeparator());
+            writer.write("        <title>Сконвертированный файл из CSV в HTML</title>" + System.lineSeparator());
+            writer.write("    </head>" + System.lineSeparator());
+            writer.write("    <body>" + System.lineSeparator());
+            writer.write("        <table>" + System.lineSeparator());
 
             String line;
-            boolean cellInQuotes = false;
+            boolean isCellInQuotes = false;
 
             while ((line = reader.readLine()) != null) {
-                if (!cellInQuotes) {
-                    writer.write("      <tr>" + System.lineSeparator());
-                    writer.write("          <td>");
+                if (!isCellInQuotes) {
+                    writer.write("            <tr>" + System.lineSeparator());
+                    writer.write("                <td>");
                 }
 
                 for (int i = 0; i < line.length(); ++i) {
                     char c = line.charAt(i);
 
                     if (c == '"') {
-                        if (!cellInQuotes) {
-                            cellInQuotes = true;
+                        if (!isCellInQuotes) {
+                            isCellInQuotes = true;
                         } else {
                             if (i != line.length() - 1 && line.charAt(i + 1) == '"') {
                                 writer.write("\"");
                                 i++;
                             } else {
-                                cellInQuotes = false;
+                                isCellInQuotes = false;
                             }
                         }
                     } else if (c == ',') {
-                        if (!cellInQuotes) {
+                        if (!isCellInQuotes) {
                             writer.write("</td>" + System.lineSeparator());
-                            writer.write("          <td>");
+                            writer.write("                <td>");
                         } else {
                             writer.write(c);
                         }
@@ -71,19 +71,19 @@ public class CsvToHtmlConverter {
                     }
                 }
 
-                if (!cellInQuotes) {
+                if (!isCellInQuotes) {
                     writer.write("</td>" + System.lineSeparator());
-                    writer.write("      </tr>" + System.lineSeparator());
+                    writer.write("            </tr>" + System.lineSeparator());
                 } else {
                     writer.write("<br/>");
                 }
             }
 
-            writer.write("    </table>" + System.lineSeparator());
-            writer.write("  </body>" + System.lineSeparator());
+            writer.write("        </table>" + System.lineSeparator());
+            writer.write("    </body>" + System.lineSeparator());
             writer.write("</html>" + System.lineSeparator());
         } catch (IOException e) {
-            System.out.println("Не удалось записать файл");
+            System.out.println("Ошибка при чтении файла");
         }
     }
 }
--- CsvToHtmlConverter/src/ru/academits/yudina/csv_to_html_converter_main/Main.java
+++ CsvToHtmlConverter/src/ru/academits/yudina/csv_to_html_converter_main/Main.java
@@ -2,14 +2,30 @@ package ru.academits.yudina.csv_to_html_converter_main;
 
 import ru.academits.yudina.csv_to_html_converter.CsvToHtmlConverter;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        String inputUrl = args[0];
-        //"C:/Users/anuta/IdeaProjects/OopCourse/CsvToHtmlConverter/InputFile3.csv";
-        String outputUrl = args[1];
-        //"C:/Users/anuta/IdeaProjects/OopCourse/CsvToHtmlConverter/OutputFile.html";
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Аргументы отсутствуют или переданы не в нужном количестве. " + System.lineSeparator() +
+                    "Необходимо передать 2 аргумента: " + System.lineSeparator() +
+                    "1. путь к файлу с расширением csv, который нужно сконвертировать; " + System.lineSeparator() +
+                    "2. путь к файлу с расширением html, куда записывается результат конвертации.");
+        }
 
-        CsvToHtmlConverter csv = new CsvToHtmlConverter(inputUrl, outputUrl);
-        csv.writeHtmlFile();
+        String inputFilePath = args[0];
+        String outputFilePath = args[1];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+            CsvToHtmlConverter csv = new CsvToHtmlConverter(inputFilePath, outputFilePath);
+            csv.writeHtmlFile(reader, writer);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла");
+        } catch (Exception e) {
+            System.out.println("Непредвиденная ошибка");
+        }
     }
 }
